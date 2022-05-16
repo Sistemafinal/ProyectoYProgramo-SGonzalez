3dcd34b7e7ba5b5f2bc34e9b97097602488f4211
Sistemafinal <56214112+Sistemafinal@users.noreply.github.com>
2022-05-16 00:14:28 -0300

    logoAP colocacion en header

--- ProyectoYProgramo-SGonzalez/src/app/app.module.ts
+++ ProyectoYProgramo-SGonzalez/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoArgProgComponent
   ],
   imports: [
     BrowserModule,
